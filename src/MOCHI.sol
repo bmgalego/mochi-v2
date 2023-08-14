a0d6645bd43bf88aaa6e742108836765e5f37c6e
Bruno Galego <bmgalego@gmail.com>
2023-08-14 11:26:09 +0100

    fix initial supply

--- src/MOCHI.sol
+++ src/MOCHI.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.13;
 
 /**
- * __  __  _____  ___  _   _  ____ 
+ * __  __  _____  ___  _   _  ____
  * (  \/  )(  _  )/ __)( )_( )(_  _)
- *  )    (  )(_)(( (__  ) _ (  _)(_ 
+ *  )    (  )(_)(( (__  ) _ (  _)(_
  * (_/\/\_)(_____)\___)(_) (_)(____)
- *                                     
+ *
  *  https://t.me/mochientry
  */
 
@@ -14,6 +14,6 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract MOCHI is ERC20("Mochi", "MOCHI", 8) {
     constructor() {
-        _mint(msg.sender, 100_000_00e8);
+        _mint(msg.sender, 100_000_000e8);
     }
 }
