b14d963cbdae8ef82d5127e4e5f7639d10c63458
Bruno Galego <bmgalego@gmail.com>
2023-08-14 02:13:40 +0100

    fix name

--- src/MOCHI.sol
+++ src/MOCHI.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
-contract MOCHI is ERC20("MOCHI", "MOCHI", 8) {
+contract MOCHI is ERC20("Mochi", "MOCHI", 8) {
     constructor() {
         _mint(msg.sender, 100_000_00e8);
     }
