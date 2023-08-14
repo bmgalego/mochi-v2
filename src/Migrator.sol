9a7b9a1bbf948a4e9f598a51cf5045dcc23a2a6a
Bruno Galego <bmgalego@gmail.com>
2023-08-14 13:33:41 +0100

    replace ERC20 with IERC20 interface in Migrator

--- src/Migrator.sol
+++ src/Migrator.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+interface IERC20 {
+    function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
 
 contract MOCHIMigrator {
-    ERC20 public immutable v1;
-    ERC20 public immutable v2;
+    IERC20 public immutable v1;
+    IERC20 public immutable v2;
 
     address immutable burner;
 
     event Migrated(address user, uint256 amount);
 
-    constructor(ERC20 _v1, ERC20 _v2, address _burner) {
+    constructor(IERC20 _v1, IERC20 _v2, address _burner) {
         v1 = _v1;
         v2 = _v2;
         burner = _burner;
